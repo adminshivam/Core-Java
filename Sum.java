9340f1e91e59771901353249d558f017589ca480
adminshivam <shivam.ku.gupta@gmail.com>
2024-06-17 13:24:42 +0530

    Learning Java

---
+++ Sum.java
@@ -0,0 +1,8 @@
+public class Sum {
+  static void sum(int num1, int num2) {
+    System.out.println("Sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+  }
+  public static void main(String[] args) {
+    sum(10, 20);
+  }
+}
